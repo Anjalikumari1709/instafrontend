25a47f7e63955e89c773a9afa89b16326180b392
Anjali Kumari <anjalikumari.170921@gmail.com>
2023-11-09 16:27:08 +0530

    my first changes

--- src/components/home/profile/MyPost.jsx
+++ src/components/home/profile/MyPost.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const MyPost = ({post}) => {
   return (
     <div>
-    <div>
-        <span style={{paddingTop:'2vh 0vh',marginTop:"5vh",borderTop:"1px solid  grey",display:"flex",justifyContent:"center",gap:"5vh",textTransform:"uppercase"}}>Posts</span>
+    <div style={{padding:'2vh 0vh',marginTop:"5vh",borderTop:"1px solid  grey",display:"flex",justifyContent:"center",gap:"5vh",textTransform:"uppercase"}}>
+        <span >Posts</span>
         <span>Reels</span>
         <span>Saved</span>
         <span>Tagged</span>
     </div>
     <div style={{width:"60vh", display:"grid",gridTemplateColumns:"auto auto auto",columnGap:"1vh",rowGap:"1vh"}} >
         {post.map((ele)=>{
-         return<img src={`${ele.postImages[0]}`} alt="" style={{width:"400px",height:"300px",objectFit:"cover"}}/>
+         return<img src={`${ele.postImages[0]}`} alt="" style={{width:"300px",height:"300px",objectFit:"cover"}}/>
         })}
     </div>
     </div>
